2eecf53a04c1d82759a24fbeb393ab54ae2bfa89
Sandra Hoang <sandrahoang686@gmail.com>
2024-09-03 09:46:15 -0400

    Pass in override link component

--- app/(datasets)/datasets/catalog.tsx
+++ app/(datasets)/datasets/catalog.tsx
@@ -1,7 +1,9 @@
 'use client';
 import { CatalogView, useFiltersWithQS } from '../../lib';
 import { useRouter } from "next/navigation";
-import { usePathname } from 'next/navigation'
+import { usePathname } from 'next/navigation';
+import Link from 'next/link'
+import styled from 'styled-components';
 
 export default function Catalog({
   datasets
@@ -12,5 +14,12 @@ export default function Catalog({
   const pathName = usePathname();
   const controlVars = useFiltersWithQS({navigate: router, push: true});
 
-  return <CatalogView datasets={datasets} onFilterChanges={() => controlVars} location={pathName}/>
+  return (
+    <CatalogView
+      datasets={datasets}
+      onFilterChanges={() => controlVars}
+      location={pathName}
+      OverrideLinkElement={Link}
+    />
+  );
 }
