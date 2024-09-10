e4a62e307335f1da15b9fade16287902a691e5d2
Sandra Hoang <sandrahoang686@gmail.com>
2024-09-10 11:21:52 -0400

    Update nav file

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -1,16 +1,25 @@
 import Link from 'next/link'
 
-const navItems = {
-  '/': {
-    name: 'home',
-  },
-  '/datasets': {
-    name: 'datasets',
-  },
-  '/stories': {
-    name: 'stories',
-  }
+interface NavItem { // @TODO: To be replaced with exposed NavItem type from veda-ui
+  path: string;
+  title: string;
+  subItems?: NavItem[];
 }
+
+const navItems: NavItem[] = [
+  {
+    path: '/',
+    title: 'Home',
+  },
+  {
+    path: '/data-catalog',
+    title: 'Data Catalog',
+  },
+  {
+    path: '/stories',
+    title: 'Stories',
+  },
+]
 
 export function Navbar() {
   return (
@@ -21,14 +30,14 @@ export function Navbar() {
           id="nav"
         >
           <div className="flex flex-row space-x-0 pr-10">
-            {Object.entries(navItems).map(([path, { name }]) => {
+            {navItems.map(({path, title}) => {
               return (
                 <Link
                   key={path}
                   href={path}
                   className="transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative py-1 px-2 m-1"
                 >
-                  {name}
+                  {title}
                 </Link>
               )
             })}
