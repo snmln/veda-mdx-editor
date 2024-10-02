fefff6c4eecbc23b01992898d25955d0532bd10b
Sandra Hoang <sandrahoang686@gmail.com>
2024-10-02 14:10:23 -0400

    Update imports for veda-ui to be in one place

--- app/components/mdx-components/block.tsx
+++ app/components/mdx-components/block.tsx
@@ -1,10 +1,10 @@
 'use client'
 
-import { Block, Prose, Figure, Caption, Image, MapBlock, CompareImage } from "@developmentseed/veda-ui"; // This returns undefined
 import { useDataStore } from "app/store/providers/data";
-import React from 'react'
+import { MapBlock } from '../../lib';
+import React from 'react';
 
-function EnhancedMapBlock(props) {
+export default function EnhancedMapBlock(props) {
   const { datasets } = useDataStore();
 
   const transformed = datasets?.map((dataset) => ({
@@ -16,14 +16,4 @@ function EnhancedMapBlock(props) {
   return (
     <MapBlock {...props} datasets={transformed}/>
   );
-}
-
-export {
-  Block,
-  Prose,
-  Image,
-  EnhancedMapBlock,
-  Caption,
-  Figure,
-  CompareImage,
 }
--- app/lib/index.ts
+++ app/lib/index.ts
@@ -1,8 +1,45 @@
 'use client';
-import { CatalogView, PageHero, useFiltersWithQS } from '@developmentseed/veda-ui';
-
-export {
+import { 
   CatalogView,
   PageHero,
-  useFiltersWithQS
+  useFiltersWithQS,
+  ReactQueryProvider,
+  DevseedUiThemeProvider,
+  PageMainContent,
+  Block,
+  Prose,
+  Figure,
+  Caption,
+  Image,
+  MapBlock,
+  CompareImage,
+} from '@developmentseed/veda-ui';
+
+/**
+ * This file is to...
+ * Quickly identify client specific library components being used throughout this application
+ * And to mark these with the client side directive so other parts of the page are still Server Components and can stay as-is
+ */
+
+export {
+  // Providers
+  DevseedUiThemeProvider,
+  ReactQueryProvider,
+
+  // Components
+  CatalogView,
+  PageHero,
+  PageMainContent,
+
+  // MDX Components
+  Block,
+  Prose,
+  Figure,
+  Caption,
+  Image,
+  MapBlock,
+  CompareImage,
+  
+  // Hooks
+  useFiltersWithQS,
 }
