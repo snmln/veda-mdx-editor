4e63c623e4065523ed05b8495d50de7c4a0a4536
Sandra Hoang <sandrahoang686@gmail.com>
2024-10-06 23:53:00 -0400

    Added PageHeader & nav

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -1,51 +1,53 @@
-import React from 'react';
 import Link from 'next/link';
+import { PageHeader, LogoContainer } from '@lib';
+import NasaLogo from '../../public/images/nasa-logo';
 
-interface NavItem {
-  // @TODO: To be replaced with exposed NavItem type from veda-ui
-  path: string;
-  title: string;
-  subItems?: NavItem[];
-}
-
-const navItems: NavItem[] = [
+const navItems = [ // @TODO: This should use the NavLinkType from veda-ui...
   {
-    path: '/',
-    title: 'Home',
+    title: 'home',
+    to: '/',
+    type: 'internalLink'
   },
   {
-    path: '/data-catalog',
-    title: 'Data Catalog',
+    title: 'datasets',
+    to: '/datasets',
+    type: 'internalLink'
   },
   {
-    path: '/stories',
-    title: 'Stories',
-  },
-];
+    title: 'stories',
+    to: '/stories',
+    type: 'internalLink'
+  }
+]
 
 export function Navbar() {
+  const linkProps = {
+    LinkElement: Link,
+    pathAttributeKeyName: 'href'
+  }
+
+  const Logo: JSX.Element = (
+    <LogoContainer 
+      linkProperties={linkProps}
+      logo={<NasaLogo />}
+      title="Earthdata"
+      subTitle="veda dashboard"
+      version="1.0.0"
+    />
+  )
+
   return (
-    <aside className='-ml-[8px] mb-16 tracking-tight'>
-      <div className='lg:sticky lg:top-20'>
+    <aside className="-ml-[8px] mb-16 tracking-tight">
+      <div className="lg:sticky lg:top-20">
         <nav
-          className='flex flex-row items-start relative px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative'
-          id='nav'
+          className="flex flex-row items-start relative px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative"
+          id="nav"
         >
-          <div className='flex flex-row space-x-0 pr-10'>
-            {navItems.map(({ path, title }) => {
-              return (
-                <Link
-                  key={path}
-                  href={path}
-                  className='transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative py-1 px-2 m-1'
-                >
-                  {title}
-                </Link>
-              );
-            })}
+          <div className="flex flex-row space-x-0 pr-10">
+          <PageHeader mainNavItems={navItems} subNavItems={[]} logo={Logo} linkProperties={linkProps} />
           </div>
         </nav>
       </div>
     </aside>
-  );
+  )
 }
