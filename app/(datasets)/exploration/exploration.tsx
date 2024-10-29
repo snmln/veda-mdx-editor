dba4d2a1a130cea6a85e01e81cabe7ada83cc699
Sandra Hoang <sandrahoang686@gmail.com>
2024-10-29 10:22:26 -0400

    test for preview for ea changes

--- app/(datasets)/exploration/exploration.tsx
+++ app/(datasets)/exploration/exploration.tsx
@@ -1,7 +1,8 @@
 'use client';
 import React, { useState } from 'react';
 import { useAtom } from 'jotai';
-import { ExplorationAndAnalysis, timelineDatasetsAtom } from 'app/lib';
+import Link from 'next/link';
+import { ExplorationAndAnalysis, timelineDatasetsAtom, DatasetSelectorModal } from 'app/lib';
 
 export default function ExplorationAnalysis({
   datasets
@@ -10,11 +11,52 @@ export default function ExplorationAnalysis({
 }) {
   const [explorationDatasets, setExplorationDatasets] = useAtom(timelineDatasetsAtom);
   const [datasetModalRevealed, setDatasetModalRevealed] = useState(
-    !datasets.length
+    !!datasets.length
   );
-  const openModal = () => setDatasetModalRevealed(true);
+
+  
+  const openModal = () => {setDatasetModalRevealed(true);}
+  const closeModal = () => {setDatasetModalRevealed(false);}
+  const transformData = () => {
+    const data = datasets?.map((post) => ({
+      ...post.metadata
+    }));
+
+    const result = data?.map((d) => {
+      const updatedTax = d.taxonomy.map((t) => {
+        const updatedVals = t.values.map((v) => {
+          return {
+            id: v.replace(/ /g, '_').toLowerCase(),
+            name: v
+          }
+        })
+        return {...t, values: updatedVals}
+      })
+      return {...d, taxonomy: updatedTax}
+    })
+
+    return result
+  };
+
+  const transformed = transformData();
 
   return (
+    <>
+    {
+      datasetModalRevealed && (
+        <DatasetSelectorModal
+          revealed={datasetModalRevealed}
+          close={closeModal}
+          linkProperties={{
+            LinkElement: Link,
+            pathAttributeKeyName: 'href'
+          }}
+          datasetPathName={'data-catalog'}
+          datasets={transformed}
+        />
+      )
+    }
     <ExplorationAndAnalysis datasets={explorationDatasets} setDatasets={setExplorationDatasets} openDatasetsSelectionModal={openModal} />
+    </>
   )
 };
