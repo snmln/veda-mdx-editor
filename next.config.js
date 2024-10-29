9fe4222ed03f3813e97f745682656d6028316883
Gjore Milevski <dzole0311@gmail.com>
2024-10-29 13:47:49 +0100

    Persist atom state with the veda-ui and remove jotai dependency

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 module.exports = {
   typescript: {
     // !! WARN !!
@@ -13,5 +15,21 @@ module.exports = {
         destination: '/:path*',
       },
     ];
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      // The following aliases are added to ensure that both the Next.js instance and
+      // the `veda-ui` library use the same instances of Jotai for state management.
+      // This resolves the issue of "Detected multiple Jotai instances," which can cause
+      // unexpected behavior due to multiple instances of Jotai's default store.
+      // For more details, refer to the GitHub discussion:
+      // https://github.com/pmndrs/jotai/discussions/2044
+      'jotai': path.resolve(__dirname, 'node_modules', 'jotai'),
+      'jotai-devtools': path.resolve(__dirname, 'node_modules', 'jotai-devtools'),
+      'jotai-location': path.resolve(__dirname, 'node_modules', 'jotai-location'),
+      'jotai-optics': path.resolve(__dirname, 'node_modules', 'jotai-optics'),
+    };
+    return config;
   },
 }
