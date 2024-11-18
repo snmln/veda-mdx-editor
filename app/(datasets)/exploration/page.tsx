5fed19eae283e67f64bd354c6171572a59e920c9
Gjore Milevski <dzole0311@gmail.com>
2024-11-18 08:47:12 +0100

    Adjust E&A page to match what we have on earthdata.nasa.gov

--- app/(datasets)/exploration/page.tsx
+++ app/(datasets)/exploration/page.tsx
@@ -1,17 +1,28 @@
-import { Suspense } from 'react';
+import React, { Suspense } from 'react';
 import { getDatasets } from 'app/content/utils/mdx';
 import ExplorationAnalysis from './exploration';
 import { PageHero } from 'app/lib';
 
 export default function Page() {
   const datasets: any[] = getDatasets();
-  // @TODO: Investigate why we need to set 100vh
   return (
-    <div style={{ minHeight: '100vh', height: '1px' }}>
-      <Suspense fallback={<>Loading...</>}>
-        <PageHero title='Exploration' isHidden />
-        <ExplorationAnalysis datasets={datasets} />
-      </Suspense>
-    </div>
+    <section>
+      <div
+        // The below styles adjust the E&A page to match what we have on earthdata.nasa.gov
+        // Ideally, we would replace some of the custom styles with the USWDS grid and util classes
+        // but since we do not have USWDS yet in the template instance, this is a quick workaround
+        // to make the page look closer to the current E&A page.
+        style={{
+          height: 'calc(100vh - 64px)',
+          position: 'absolute',
+          width: '100%',
+        }}
+      >
+        <Suspense fallback={<>Loading...</>}>
+          <PageHero title='Exploration' isHidden />
+          <ExplorationAnalysis datasets={datasets} />
+        </Suspense>
+      </div>
+    </section>
   );
 }
