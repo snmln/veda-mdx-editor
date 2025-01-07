2319efc576544d3358455bed153e14fe5a465408
Sandra Hoang <sandrahoang686@gmail.com>
2025-01-07 06:59:51 -0500

    update homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,22 @@
 import React from 'react';
+import Link from 'next/link';
 
-export default function Page() {
+export default function HomePage() {
   return (
     <section>
       <h1 className='mb-8 text-2xl font-semibold tracking-tighter'>
-        Next.js VEDA Template Instance
+        VEDA Dashboard Template Instance
       </h1>
 
-      <p className='mb-4'>
-        This is a test Next JS instance to figure out how to use VEDA specific
-        MDX files with NEXT JS Instance.
+      <p>
+        The Visualization, Exploration, and Data Analysis (VEDA) platform is intended to show the capabilities of Analysis-Ready, Cloud-Optimized (ARCO) data. The VEDA Dashboard is one interface for the VEDA platform, that allows science teams to showcase their work for a variety of stakeholders. This flexible dashboard can be customized for your own needs. To learn more about creating your own self-hosted instance of the VEDA Dashboard, and how to manage it, visit our <Link href="https://docs.openveda.cloud/instance-management/" className="text-neutral-600 dark:text-neutral-300"> VEDA Documentation</Link>.
       </p>
+      <br/>
+      <p>
+        This is a template for what your own instance of the VEDA Dashboard might look like. It includes the core VEDA Dashboard features, compiled using Next JS. You can copy this template instance to start your own custom instance of the VEDA Dashboard. For more help with this, reach out to veda@uah.edu. 
+      </p>
+
+      
     </section>
   );
 }
