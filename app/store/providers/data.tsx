b740d9f1257f7b9b27acb4269f426b42f3db7430
ajinkyakulkarni <ajinkya@ajinkyakulkarni.com>
2025-01-14 15:21:59 -0500

    Explicitly parse mapLable as funciton on client side (#31)
    * Explicitly parse mapLable as funciton on client side
    
    * Replace comment

--- app/store/providers/data.tsx
+++ app/store/providers/data.tsx
@@ -14,6 +14,24 @@ export function useDataStore() {
   return useContext<DataStore>(DataContext);
 }
 
+// @TODO: Decided how to handle function as mapLabel from VEDA UI
+// https://github.com/NASA-IMPACT/veda-ui/issues/1377
+function updateMapLabels(data) {
+  return data.map((dataset) => {
+    if (dataset.metadata && dataset.metadata.layers) {
+      dataset.metadata.layers.forEach((layer) => {
+        if (layer.mapLabel) {
+          layer.mapLabel = eval(layer.mapLabel);
+        }
+        if (layer.compare && layer.compare.mapLabel) {
+          layer.compare.mapLabel = eval(layer.compare.mapLabel);
+        }
+      });
+    }
+    return dataset;
+  });
+}
+
 function DataProvider({
   initialDatasets = undefined,
   children,
@@ -21,7 +39,9 @@ function DataProvider({
   children: JSX.Element | ReactNode;
   initialDatasets: any[] | undefined;
 }) {
-  const [datasets, setDatasets] = useState<any[] | undefined>(initialDatasets);
+  const [datasets, setDatasets] = useState<any[] | undefined>(
+    updateMapLabels(initialDatasets),
+  );
   const value = {
     datasets,
     setDatasets,
