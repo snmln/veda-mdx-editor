659416d07e1f3cc2e94a9177ba12d17f0e89a496
Alice Rühl <alice.ruehl@gmail.com>
2025-01-28 13:05:31 +0100

    Provide footer settings to avoid error
    It's unclear why TypeScript doesn't complain when this prop is missing. Further investigation might be needed.

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -5,13 +5,25 @@ import NasaLogoColor from './nasa-logo-color';
 import { PageFooter } from '@lib';
 
 export default function Footer() {
+  const defaultFooterSettings = {
+    secondarySection: {
+      division: 'NASA EarthData 2024',
+      version: 'BETA VERSION',
+      title: 'NASA Official',
+      name: 'Manil Maskey',
+      to: 'test@example.com',
+      type: 'email',
+    },
+    returnToTop: true,
+  };
   return (
     <PageFooter
       mainNavItems={navItems}
       subNavItems={subNavItems}
       hideFooter={false}
-      linkProperties={{ LinkElement: Link, pathAttributeKeyName: 'href' }}
       logoSvg={<NasaLogoColor />}
+      linkProperties={{ LinkElement: Link, pathAttributeKeyName: 'href' }}
+      footerSettings={defaultFooterSettings}
     />
   );
 }
